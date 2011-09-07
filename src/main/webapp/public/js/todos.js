1656d3c4990f8303a9e425f7e2db0c6093a0a4fa
Frédéric Camblor <fcamblor@gmail.com>
2011-09-07 21:43:51 +0200

    removed line fixing problem based on IE

--- src/main/webapp/public/js/todos.js
+++ src/main/webapp/public/js/todos.js
@@ -12,7 +12,6 @@ $(function() {
             task.addClass("edit");
             // FIXME : uncomment following lines for step 3
             checkbox.attr('checked', task.hasClass('done'));
-            checkbox.appendTo(task.find(".action"));
 
             var node = document.getElementById('checkTask');
             if(node === null){
