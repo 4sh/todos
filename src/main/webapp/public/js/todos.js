562d77dfd0aed9e775acc347233a43625f430a51
Xavier Hanin <xavier.hanin@gmail.com>
2012-04-10 09:58:41 +0200

    back to step 0

--- src/main/webapp/public/js/todos.js
+++ src/main/webapp/public/js/todos.js
@@ -2,26 +2,26 @@ $(function() {
     var checkbox = $("<input type='checkbox' id='checkTask'>");
 
     // FIXME : to uncomment for step 1
-    $(".task .title").live('click', function() {
-        var task = $(this).closest('.task');
-        if (task.hasClass("edit")) {
-            task.removeClass("edit");
-            checkbox.detach();
-        } else {
-            $(".task").removeClass("edit");
-            task.addClass("edit");
-            //FIXME : uncomment following lines for step 3
-             checkbox.attr('checked', task.hasClass('done'));
-
-            var node = document.getElementById('checkTask');
-            if(node === null){
-                checkbox.appendTo(task.find(".action"));
-            // FIXME : remove this else section for step 4
-            } else {
-                document.getElementById(task.attr("id")).getElementsByTagName('td')[0].appendChild(node);
-            }
-        }
-    });
+//    $(".task .title").live('click', function() {
+//        var task = $(this).closest('.task');
+//        if (task.hasClass("edit")) {
+//            task.removeClass("edit");
+//            checkbox.detach();
+//        } else {
+//            $(".task").removeClass("edit");
+//            task.addClass("edit");
+//            // FIXME : uncomment following lines for step 3
+//            // checkbox.attr('checked', task.hasClass('done'));
+//
+//            var node = document.getElementById('checkTask');
+//            if(node === null){
+//                checkbox.appendTo(task.find(".action"));
+//            // FIXME : remove this else section for step 4
+//            } else {
+//                document.getElementById(task.attr("id")).getElementsByTagName('td')[0].appendChild(node);
+//            }
+//        }
+//    });
 
 
     $("#newTask").click(function () {
